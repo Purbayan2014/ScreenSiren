8842f337ed2f6c9367beac89e7679618a855658a
purbayan2014 <purbayan2014@gmail.com>
2023-04-25 12:37:43 +0530

    rendering json request for mockup in home components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movies: any = [];
+  constructor(private moviesServices: MoviesService) {}
 
+  ngOnInit(): void {
+    // return the observable
+    // .subscribe means to wait for the function to fetch the data
+    // from the rest api
+    this.moviesServices.getMovies().subscribe((response: any) => {
+      this.movies = response.results;
+      console.log(this.movies);
+    });
+  }
 }
