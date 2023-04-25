a9162ac3eed4865690db232e35532b007e2098fc
purbayan2014 <purbayan2014@gmail.com>
2023-04-25 09:55:54 +0530

    footer added

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  // adding the dynamic date variable for footer
+  date: Date = new Date();
+  constructor() {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
