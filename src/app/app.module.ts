56642507f1ad1419339e5524aba35c628b09db93
purbayan2014 <purbayan2014@gmail.com>
2023-04-25 12:21:27 +0530

    service for fetching popular movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -9,18 +8,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    MoviesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/enviornment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  // service for getting the popular movies from the rest api
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getMovies() {
+    // function which returns the movies using a get request
+    return this.http.get(`https://api.themoviedb.org/3/movie/popular?api_key=${environment.moviedbapi}`);
+  }
 }
