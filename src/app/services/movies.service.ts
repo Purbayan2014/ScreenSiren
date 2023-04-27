3c8ccf1e588d4b3e3b39b69db7ce41eb548691c0
purbayan2014 <purbayan2014@gmail.com>
2023-04-27 22:58:28 +0530

    get requests to fetch latest movies and tv shows

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/enviornment';
+import { MovieDto } from '../models/movie';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { TvDto } from '../models/tv';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
-  // service for getting the popular movies from the rest api
+    // service for getting the popular movies from the rest api
+    // function which returns the movies using a get request
+  baseUrl: string = 'https://api.themoviedb.org/3';
+
   constructor(private http: HttpClient) {}
 
-  getMovies() {
-    // function which returns the movies using a get request
-    return this.http.get(`https://api.themoviedb.org/3/movie/popular?api_key=${environment.moviedbapi}`);
+  getMovies(type: string = 'upcoming', count: number = 12) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${environment.moviedbapi}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
+  }
+
+  getTvs(type: string = 'latest', count: number = 12) {
+    return this.http.get<TvDto>(`${this.baseUrl}/tv/${type}?api_key=${environment.moviedbapi}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
   }
 }
